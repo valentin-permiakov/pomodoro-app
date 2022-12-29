f960b32a6e5c61cdc128d9ecfa5e95b010cd321a
Valentin Permiakov <valentin.permiakov92@gmail.com>
2022-12-29 15:50:00 +0100

    ready for gh pages

--- src/icons/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './PomodoroLogo'
-export * from './Icon'
-
--- src/utils/formatWorkTime.ts
+++
@@ -1,8 +0,0 @@
-export const formatWorkTime = (seconds: number) => {
-  const h = Math.floor(seconds / 3600);
-  const m = Math.floor((seconds % 3600) / 60);
-
-  const hDisplay = h > 0 ? h + ' час ' : '';
-  const mDisplay = m > 0 ? m + ' мин' : '';
-  return hDisplay + mDisplay;
-};
